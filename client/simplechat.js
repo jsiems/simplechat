852469c71205b3a26b5cae9f650d98ec35a93570
josh <joshuasiems@gmail.com>
2018-02-04 21:00:19 +0000

    Update code to add comments, clean it up

--- client/simplechat.js
+++ client/simplechat.js
@@ -4,6 +4,23 @@ var room = "";
 var lines = 0;
 var maxLines = 100;
 
+$(document).ready(function() {
+  $(window).resize(function() {
+    updateScroll(true);
+  });
+
+  $("#message").keyup(function(e){
+    var code = e.keyCode ? e.keyCode : e.which;
+    if(code == 13) {
+      sendMessage();
+    }
+  });
+
+  $("#connectButton").click(connect);
+  $("#disconnectButton").click(disconnect);
+  $("#sendMessageButton").click(sendMessage);
+});
+
 function connect() {
   //declares global websocket that can be used everywhere
   ws = new WebSocket('ws://' + document.location.hostname, 'chat');
@@ -13,24 +30,30 @@ function connect() {
     username = $("#usernameTextarea").val();
     room = $("#roomTextarea").val();
     lines = 0;
-    $("#connectDiv").hide();
-    $("#connectedDiv").show();
+    $("#connectDiv").css("visibility", "hidden");
+    $("#connectedDiv").css("visibility", "visible");
     $("#usernameDiv").html('username: ' + username);
     $("#roomDiv").html('room: ' + room);
-    //practice sending other things later
 
     var data = { action: 'connect', username: username, room: room};
     ws.send(JSON.stringify(data));
   }
 
   ws.onmessage = function(msg) {
+    //parses the data from the messages
     var message = JSON.parse(msg.data);
+
+    //cuts off the top line of the messages if there are too many
     var conversation = $("#messageBox").html();
     lines ++;
     if(lines > maxLines) {
       lines --;
       conversation = conversation.substring(0, conversation.lastIndexOf('<span'));
     }
+
+    //Puts the message in the HTML with special formatting
+    //  if from server - italics
+    //  if you posted the message - bold
     $("#messageBox").html("<span"
          + (message.username == 'server' ? " class='server'" : "")
          + (message.username == username ? " class='me'" : "")
@@ -40,6 +63,7 @@ function connect() {
   }
 }
 
+//transmits a message to the server
 function sendMessage() {
   if(!connected)
     return;
@@ -49,13 +73,15 @@ function sendMessage() {
   $("#message").focus();
 }
 
+//Disconnectes the user
 function disconnect() {
   ws.close();
-  $("#connectedDiv").hide();
-  $("#connectDiv").show();
+  $("#connectedDiv").css("visibility", "hidden");
+  $("#connectDiv").css("visibility", "visible");
   $("#messageBox").html("");
 }
 
+//scrolls down automatically when messages appear at bottom of box
 function updateScroll(mandatory) {
   if($("#messageBox")[0].scrollHeight - $("#messageBox").scrollTop() - $("#messageBox").height() < 200 || mandatory) {
     $("#messageBox").scrollTop($("#messageBox")[0].scrollHeight - $("#messageBox").height());
--- server/chatroom.js
+++ server/chatroom.js
@@ -3,6 +3,7 @@ var rooms = {};
 
 module.exports = {
   addClient: function(ws) {
+    //there is a better way to do this
     ws.id = nextId;
     nextId ++;
 
@@ -10,7 +11,9 @@ module.exports = {
     ws.on('message', function(message) {
       var data = JSON.parse(message);
 
+      //a user wants to connect to a room
       if(data.action == "connect") {
+        //if a room with this name has not been created, it creates one
         if(rooms[data.room] == undefined) {
           rooms[data.room] = new Room(data.room);
         }
@@ -18,6 +21,9 @@ module.exports = {
         ws.room = data.room;
         rooms[data.room].connect(ws);
       }
+
+      //a user wants to send a message to a room
+      //  transmits to EVERYONE in the room (INCLUDING USER THAT SENT IT)
       if(data.action == "send_message") {
         var response = {};
         response.username = data.username;
@@ -26,11 +32,14 @@ module.exports = {
       }
     });
 
+    //user disconnects
     ws.on('close', function(){
       for(var key in rooms) {
         if(rooms.hasOwnProperty(key))  {
           if(rooms[key].getName() == ws.room) {
             rooms[key].disconnect(ws);
+
+            //if no more members in the room, it is deleted
             if(rooms[key].getUserCount() == 0) {
               rooms[key] = null;
               delete rooms[key];
@@ -43,6 +52,9 @@ module.exports = {
   }
 }
 
+//Room object is created each time a new room is created
+//  stores number of users, the name of the room, and 
+//  a list of websocket objects that are connected to this room
 function Room(nameInput) {
   var users = 0;
   var name = nameInput;
@@ -51,6 +63,7 @@ function Room(nameInput) {
   this.getName = function() {return name};
   this.getUserCount = function() {return users};
 
+  //stores the new connection in the array of web sockets
   this.connect = function(ws) {
     websockets[ws.id] = ws;
     var data = {};
@@ -60,6 +73,7 @@ function Room(nameInput) {
     users ++;
   }
 
+  //removes a websocket from the room
   this.disconnect = function(ws) {
     websockets[ws.id] = null;
     delete websockets[ws.id];
@@ -70,6 +84,7 @@ function Room(nameInput) {
     users --;
   }
 
+  //sends a message to every user in the room
   this.sendMessage = function(data) {
     for(var key in websockets) {
       if(websockets.hasOwnProperty(key)) {
